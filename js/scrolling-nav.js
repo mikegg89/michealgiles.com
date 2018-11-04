9bb1e8ee76d191300a89b217570276cc2104c87b
Micheal Giles <mikegg21@gmail.com>
2018-11-03 22:54:54 -0500

    clean up

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -3,7 +3,5 @@ $('.option').children('a').click(function (e) {
     var postId = $($(this).attr('href'));
     var postLocation = postId.offset().top - 60;
 
-    console.log(postLocation);
-
     $('html, body').animate({scrollTop:postLocation},'slow');
 });
