a90eff2da3b4f0ddc5da973d29095ccd00b2a5ba
Micheal Giles <mikegg21@gmail.com>
2018-11-03 22:30:44 -0500

    fixed scrolling issue

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,7 +1,9 @@
 $('.option').children('a').click(function (e) {
     e.preventDefault();
     var postId = $($(this).attr('href'));
-    var postLocation = postId.offset().top;
+    var postLocation = postId.offset().top - 60;
+
+    console.log(postLocation);
 
     $('html, body').animate({scrollTop:postLocation},'slow');
 });
